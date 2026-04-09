7fe098d1a7ef8b70b1b6c3a5a05e12433913e45a
ysh810 <yseungh0810@gmail.com>
2026-04-09 15:43:49 +0900

    secret test3

--- secret_app.py
+++ secret_app.py
@@ -1,6 +1,6 @@
 import os
 secret_value = os.getenv("SECRET_MESSAGE")
-secret_value2 = os.getenv("SECRET_MESSAGE")
+secret_value2 = os.getenv("SECRET_MESSAGE2")
 
 
 if secret_value:
@@ -9,6 +9,6 @@ else:
     print("SECRET_MESSAGE 값이 없음")
 
 if secret_value2:
-    print("SECRET_MESSAGE 값 읽기 성공")
+    print("SECRET_MESSAGE2 값 읽기 성공")
 else:
-    print("SECRET_MESSAGE 값이 없음")
+    print("SECRET_MESSAGE2 값이 없음")
